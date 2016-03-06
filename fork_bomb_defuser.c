ffd141b23e9e668a68022750e774ab89a8ee6fce
Adam Mastov <amastov1@gmail.com>
2016-03-05 18:28:58 -0700

    Initially implement fork bomb detector.

--- fork_bomb_defuser.c
+++ fork_bomb_defuser.c
@@ -3,6 +3,8 @@
 #include <linux/init.h>
 #include <linux/kthread.h>
 #include <linux/mutex.h>
+#include <linux/sched.h>
+#include <sys/syscall.h>
 
 static struct task_struct *thread_fbm;    /* fork bomb monitoring task */
 static struct task_struct *thread_fbk;    /* fork bomb killing task */
@@ -11,6 +13,26 @@ static struct task_struct *thread_fbk;    /* fork bomb killing task */
 DEFINE_MUTEX(fb_lock);
 unsigned long bomb_pid = 0;    /* from test fb_defuser may need different */
                          /* buffer for actual monitor to killer communication */
+
+inline int detect_fb(){
+	unsigned long cur_time = 0;
+	int forks_per_sec = 0;
+	struct task_struct *p;
+
+	cur_time = jiffies;
+	
+	read_lock(&tasklist_lock);
+	for_each_task(p){
+		if(p->uid == current->uid){
+			if((cur_time-(p->start_time)) <= 100){
+				forks_per_sec++;
+			}
+	}
+	if(forks_per_sec > 50){
+		return current->uid;
+	}
+	return 0;
+}
 
 // Function executed by kernel thread
 static int fb_monitor(void *unused)
