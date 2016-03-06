0146fdc4e09298230e8c2c29fbd15100942426c9
Chris Monken <cmonken@asu.edu>
2016-03-06 14:33:11 -0700

    test fork bomb that only creates 2 forks then stays active until terminal is closed to look at in 'ps -e'

---
+++ test_fork.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <stdlib.h> 
+
+int main()
+{
+//    int i = 0;
+    int pid = 0;
+    
+/*    while(i < 5)
+    for(i = 0; i < 6; i++)
+    {
+        pid = fork();  
+        printf("Forking\n");
+        i++;
+    }
+*/
+    pid = fork();  
+    printf("Forking\n");
+//    pid = fork();  
+//    printf("Forking\n");
+//    pid = fork();  
+//    printf("Forking\n");
+//    pid = fork();  
+//    printf("Forking\n");
+//    pid = fork();  
+//    printf("Forking\n");
+    
+    while(1) {}
+    return 0;
+}
